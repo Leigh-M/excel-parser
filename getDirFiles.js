1dd43e2efad55978bade60438024046eacec546c
Leigh Mathieson <leigh.mathieson@ukfuels.co.uk>
2020-09-06 15:48:58 +0100

    WIP

--- getDirFiles.js
+++ getDirFiles.js
@@ -3,9 +3,8 @@ const fs = require('fs');
 const getDirFiles = path => {
     const files = fs.readdirSync(path);
 
-    console.log(files);
-    // return files.filter(file => file.match(new RegExp(`.*.(${ extension })`, 'ig')));
+    return files.filter(file => file.match(new RegExp('.*.(xls|xlsx)', 'ig')));
 };
 
-getDirFiles('./excels');
-// module.exports = getDirFiles;
+// getDirFiles('./excels');
+module.exports = getDirFiles;
--- xlsXParser.js
+++ xlsXParser.js
@@ -1,40 +1,47 @@
 const XLSX = require('xlsx');
 
-const workbook = XLSX.readFile('testData/Copy of Excel76.xls');
-const worksheet = workbook.Sheets.Sheet1;
-const gridSize = worksheet['!ref'];
-const [range] = gridSize.match(/[A-Z]\d+$/g);
+const getDirFiles = require('./getDirFiles');
 
-const colsRegex = /[A-Z]/g;
-const rowsRegex = /\d+/g;
-
-const [highestColChar] = range.match(colsRegex);
-const [rowsLength] = range.match(rowsRegex);
-
-const colsMap = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
-];
-
-const highestColIndex = colsMap.indexOf(highestColChar);
+const files = getDirFiles('./testData');
 
 const excelData = [];
 
-for (let row = 2; row <= rowsLength; row++) {
-    const rowData = [];
+files.forEach(file => {
+    const workbook = XLSX.readFile(`testData/${ file }`);
+    const worksheet = workbook.Sheets.Sheet1;
+    const gridSize = worksheet['!ref'];
+    const [range] = gridSize.match(/[A-Z]\d+$/g);
 
-    for (let col = 0; col < highestColIndex; col++) {
-        if (worksheet[`${ colsMap[col] }${ row }`] !== undefined
-            && worksheet[`${ colsMap[col] }${ row }`].v !== undefined
-        ) {
-            const cellValue = worksheet[`${ colsMap[col] }${ row }`].v;
-            rowData.push(cellValue);
-        } else {
-            rowData.push('');
+    const colsRegex = /[A-Z]/g;
+    const rowsRegex = /\d+/g;
+
+    const [highestColChar] = range.match(colsRegex);
+    const [rowsLength] = range.match(rowsRegex);
+
+    const colsMap = [
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
+    ];
+
+    const highestColIndex = colsMap.indexOf(highestColChar);
+
+
+    for (let row = 2; row <= rowsLength; row++) {
+        const rowData = [];
+
+        for (let col = 0; col < highestColIndex; col++) {
+            if (worksheet[`${ colsMap[col] }${ row }`] !== undefined
+                && worksheet[`${ colsMap[col] }${ row }`].v !== undefined
+            ) {
+                const cellValue = worksheet[`${ colsMap[col] }${ row }`].v;
+                rowData.push(cellValue);
+            } else {
+                rowData.push('');
+            }
         }
-    }
 
-    excelData.push(rowData);
-}
+        excelData.push(rowData);
+    }
+});
 
 const wb = XLSX.utils.book_new();
 const ws = XLSX.utils.aoa_to_sheet(excelData);
